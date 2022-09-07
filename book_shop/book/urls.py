3e12aa47727a6628c23d0401fc01f62108fd77d6
bikey_33 <thapakbb1289@gmail.com>
2022-09-07 18:20:08 +0545

    Addition of cart logo and wrote views for listing books on cart

--- book_shop/book/urls.py
+++ book_shop/book/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("", views.show_book_list, name="home"),
     path("detail/<int:pk>", views.show_book_detail, name="detail"),
     path("cart_add/<int:pk>", views.add_to_cart, name="cart"),
-    path("order_add/<int:pk>", views.add_to_order, name="order"),
+    path("show_cart/",views.CartView.as_view(), name='show_cart')
+    
+    #path("order_add", views.add_to_order, name="order"),
 
 ]
